d38a58a6757dc5ec5567b22dfd23527af41d82bf
Lutho Masoka <46578144+luthomasoka@users.noreply.github.com>
2023-05-26 00:21:47 +0000

    user authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,6 +7,22 @@ from .models import Product
 from .products import products
 from .serializers import ProductSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
